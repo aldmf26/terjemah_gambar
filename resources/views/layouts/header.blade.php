957dcaf885064289015f4e5eb241edb3ed30642b
= <aldimf26@gmail.com>
2025-09-25 21:02:23 +0800

    dsa

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -23,6 +23,10 @@
                     <div class="message-body">
                         <div class="mx-3 mt-2">
                             <ul class="d-flex gap-2">
+                                @role('user')
+                                    <li><a href="{{ route('participant.dashboard') }}" class="btn btn-sm btn-outline-primary">Daftar Quiz</a></li>
+                                    <li><a href="{{ route('participant.riwayat') }}" class="btn btn-sm btn-outline-primary">Riwayat</a></li>
+                                @endrole
                                 @role(['admin', 'superadmin'])
                                 <li><a href="{{ route('admin.terjemahan.index') }}" class="btn btn-sm btn-outline-primary">Data Terjemahan</a></li>
                                 <li><a href="{{ route('quizzes.index') }}" class="btn btn-sm btn-outline-primary">Data Quiz</a></li>
